66c150e3089def11dbc6dd945df3df6106a68819
marcj04 <m.julian0440@gmail.com>
2023-01-23 23:57:35 +0000

    Second

--- hands_on_lab.py
+++ hands_on_lab.py
@@ -1,6 +1,18 @@
 # TRYIT: Write a print statement that displays both the type and value of Pi
+import math
+
+pi = math.pi
+
+print(f"The value of pi is {pi} and the type of value pi is {str(type(pi))}")
 
 # TRYIT: Write a conditional to print out if `i` is less than or greater than 50
+num = int(input("Enter value: "))
+
+if num < 50:
+    print(f"{num} is less than 50")
+else:
+    print(f"{num} is greater than 50")
+
 
 # TRYIT: Write a conditional that prints the color of the picked fruit
 
